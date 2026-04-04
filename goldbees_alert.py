2152d7f6bd74a2857915f0ae0688c78cf0f449c4
vikhil <vikhil@gmail.com>
2026-04-04 11:49:10 +0530

    Create goldbees_alert.py

---
+++ goldbees_alert.py
@@ -0,0 +1,28 @@
+import yfinance as yf
+import requests
+import os
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+def send_msg(msg):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    requests.get(url, params={"chat_id": CHAT_ID, "text": msg})
+
+data = yf.download("GOLDBEES.NS", period="1d", interval="5m")
+
+price = float(data['Close'].iloc[-1])
+
+print("Current Price:", price)
+
+if 118 <= price <= 121:
+    send_msg(f"🟢 BUY GoldBees @ ₹{price}")
+
+elif price <= 115:
+    send_msg(f"🔥 STRONG BUY GoldBees @ ₹{price}")
+
+elif price >= 133:
+    send_msg(f"🔴 SELL GoldBees @ ₹{price}")
+
+else:
+    print("No signal")
