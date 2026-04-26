3158d406389552ac091c19aecf6f681f9e9c47bc
vikhil <vikhil@gmail.com>
2026-04-26 20:48:25 +0530

    Update goldbees_alert.py

--- goldbees_alert.py
+++ goldbees_alert.py
@@ -34,16 +34,6 @@ print("CHAT_ID:", CHAT_ID)
 
 BASE_CAPITAL = 100000
 PROFIT_POOL = BASE_CAPITAL * 0.2
-
-# ===================== COLOR FUNCTION =====================
-# def get_color(decision):
-#     if "BUY" in decision or "BREAKOUT" in decision:
-#         return {"red": 0.8, "green": 1, "blue": 0.8}   # Light Green
-#     elif "PROFIT" in decision or "EXIT" in decision: #        return {"red": 1, "green": 0.8, "blue": 0.8}   # Light Red
-#     elif "HOLD" in decision:
-#         return {"red": 1, "green": 1, "blue": 0.6}     # Yellow
-#     else:
-#         return {"red": 0.9, "green": 0.9, "blue": 0.9} # Grey
         
 # ===================== TELEGRAM =====================
 def send_msg(message_text):
@@ -129,8 +119,6 @@ market_trend = "BULLISH" if nifty_price > nifty_ema else "BEARISH"
 messages = []
 updates = []
 invalid_tickers = []
-
-format_requests = []
 
 print("Script started")
 
@@ -359,15 +347,7 @@ for i, row in enumerate(data_rows, start=2):
                 buy_qty
             ]
         })
-        # ===================== COLOR TRACK =====================
-        # row_color = get_color(decision)
-
-        # format_requests.append({
-        #     "range": f"M{actual_row}",
-        #     "format": {
-        #         "backgroundColor": row_color
-        #     }
-        # })
+        
         # ================= TELEGRAM =================
         # if ("BUY" in decision or "PROFIT" in decision) and rank in ["🔥 Strong Buy", "👍 Good"]:
         if "BUY" in decision or "PROFIT" in decision:
@@ -385,32 +365,6 @@ for i, row in enumerate(data_rows, start=2):
 # print("Messages count:", len(messages))
 print(f"Updates count: {len(updates)}")
 print(f"Messages count: {len(messages)}")
-
- # ===================== APPLY COLORS =====================
- # from gspread_formatting import format_cell_ranges, CellFormat, Color
-        
-# if format_requests:
-#     print(f"Applying colors to {len(format_requests)} rows...")
-#     try:
-#         formatted_ranges = []
-
-#         for req in format_requests:
-#             r = req["range"]
-#             c = req["format"]["backgroundColor"]
-
-#             formatted_ranges.append((
-#                 r,
-#                 CellFormat(
-#                     backgroundColor=Color(c["red"], c["green"], c["blue"])
-#                 )
-#             ))
-
-#         format_cell_ranges(sheet, formatted_ranges)
-
-#         print("✅ Color formatting applied")
-
-#     except Exception as e:
-#         print("❌ Color formatting failed:", e)
 
 # ✅ ADD HERE (Telegram fix)
 #if messages:
